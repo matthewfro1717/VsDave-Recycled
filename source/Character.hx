1f4d9e387d6a0f19a1e779fb69323877d87b0f2d
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-06 15:48:58 -0500

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -1482,4 +1482,10 @@ class Character extends FlxSprite
 	{
 		animOffsets[name] = [x, y];
 	}
+	public function setScale(x:Float, y:Float)
+	{
+		scale.set(x, y);
+		width = Math.abs(x) * frameWidth;
+		height = Math.abs(y) * frameHeight;
+	}
 }
