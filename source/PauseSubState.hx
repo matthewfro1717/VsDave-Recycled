0f348f0050705a24555f351003cb38c83b99fad9
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-10 15:25:38 -0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -259,7 +259,6 @@ class PauseSubState extends MusicBeatSubstate
 				FlxG.switchState(new CharacterSelectState());
 			case "Bot Play":
 				PlayState.botPlay = false;
-				PlayState.botplaySine = false;
 				PlayState.instance.botplayTxt.alpha = 1;
 				PlayState.instance.botplayTxt.visible = false;
 			case "No Miss Mode":
