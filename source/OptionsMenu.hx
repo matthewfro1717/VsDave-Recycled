7f8408eb601f925ca88d18dbde56bb4fe99b4291
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-04-08 10:38:18 -0400

    Update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -91,6 +91,7 @@ class OptionsMenu extends MusicBeatState
 			+ "\n" + LanguageManager.getTextString('option_change_langauge')
 			+ "\n" + (FlxG.save.data.disableFps ? LanguageManager.getTextString('option_enable_fps') : LanguageManager.getTextString('option_disable_fps'))
 			+ "\n" + (CompatTool.save.data.compatMode ? LanguageManager.getTextString('option_enable_compat') : LanguageManager.getTextString('option_disable_compat'))
+			+ "\n" + (FlxG.save.data.modchart ? 'Mod Chart OFF' : 'Mod Chart ON')
 			+ "\n" + (FlxG.save.data.botplay ? 'Bot Play ON' : 'Bot Play OFF')
 			);
 
@@ -200,8 +201,11 @@ class OptionsMenu extends MusicBeatState
 					CompatTool.save.data.compatMode = !CompatTool.save.data.compatMode;
 					updateGroupControls(CompatTool.save.data.compatMode ? LanguageManager.getTextString('option_enable_compat') : LanguageManager.getTextString('option_disable_compat'), 11, 'Vertical');
 			        case 12:
+                                        if (!awaitingExploitation) FlxG.save.data.modchart = !FlxG.save.data.modchart;
+					updateGroupControls(FlxG.save.data.modchart ? 'Mod Chart OFF' : 'Mod Chart ON', 12, 'Vertical');
+			        case 13:
                                         if (!awaitingExploitation) FlxG.save.data.botplay = !FlxG.save.data.botplay;
-					updateGroupControls(FlxG.save.data.botplay ? 'Bot Play ON' : 'Bot Play OFF', 12, 'Vertical');
+					updateGroupControls(FlxG.save.data.botplay ? 'Bot Play ON' : 'Bot Play OFF', 13, 'Vertical');
 			}
 		}
 	}
